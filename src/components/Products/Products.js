1dd31781f33a38665d579b12c8b4672231ebb940
Marcin Adamiak <marcin_89@outlook.com>
2023-05-27 18:39:50 +0200

    Fix Products list

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -3,24 +3,20 @@ import productsData from '../../data/products';
 import Product from '../Product/Product';
 
 const Products = () => {
-  const [products]  = useState(productsData);
+  const [products] = useState(productsData);
 
   return (
     <section>
-      <Product
-        id={products[0].id}
-        name={products[0].name}
-        title={products[0].title}
-        colors={products[0].colors}
-        sizes={products[0].sizes}
-        basePrice={products[0].basePrice} />
-      <Product
-        id={products[1].id}
-        name={products[1].name}
-        title={products[1].title}
-        colors={products[1].colors}
-        sizes={products[1].sizes}
-        basePrice={products[1].basePrice} />
+      {products.map((product) => (
+        <Product
+          key={product.id}
+          id={product.id}
+          title={product.title}
+          colors={product.colors}
+          sizes={product.sizes}
+          basePrice={product.basePrice}
+        />
+      ))}
     </section>
   );
 };
