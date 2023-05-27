6af69807d02b8dbc10e9f5a8623057bad6d6a987
Marcin Adamiak <marcin_89@outlook.com>
2023-05-27 19:11:15 +0200

    Add PropType validator

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,15 +1,17 @@
 import styles from './Product.module.scss';
 import clsx from 'clsx';
 import Button from '../Button/Button';
+import PropTypes from 'prop-types';
 
-const Product = props => {
+const Product = (props) => {
   return (
     <article className={styles.product}>
       <div className={styles.imageContainer}>
-        <img 
+        <img
           className={styles.image}
-          alt="Kodilla shirt"
-          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`} />
+          alt='Kodilla shirt'
+          src={`${process.env.PUBLIC_URL}/images/products/shirt-kodilla--black.jpg`}
+        />
       </div>
       <div>
         <header>
@@ -20,27 +22,57 @@ const Product = props => {
           <div className={styles.sizes}>
             <h3 className={styles.optionLabel}>Sizes</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={styles.active}>S</button></li>
-              <li><button type="button">M</button></li>
-              <li><button type="button">L</button></li>
-              <li><button type="button">XL</button></li>
+              <li>
+                <button type='button' className={styles.active}>
+                  S
+                </button>
+              </li>
+              <li>
+                <button type='button'>M</button>
+              </li>
+              <li>
+                <button type='button'>L</button>
+              </li>
+              <li>
+                <button type='button'>XL</button>
+              </li>
             </ul>
           </div>
           <div className={styles.colors}>
             <h3 className={styles.optionLabel}>Colors</h3>
             <ul className={styles.choices}>
-              <li><button type="button" className={clsx(styles.colorBlack, styles.active)} /></li>
-              <li><button type="button" className={clsx(styles.colorRed)} /></li>
-              <li><button type="button" className={clsx(styles.colorWhite)} /></li>
+              <li>
+                <button type='button' className={clsx(styles.colorBlack, styles.active)} />
+              </li>
+              <li>
+                <button type='button' className={clsx(styles.colorRed)} />
+              </li>
+              <li>
+                <button type='button' className={clsx(styles.colorWhite)} />
+              </li>
             </ul>
           </div>
           <Button className={styles.button}>
-            <span className="fa fa-shopping-cart" />
+            <span className='fa fa-shopping-cart' />
           </Button>
         </form>
       </div>
     </article>
-  )
+  );
+};
+
+Product.propTypes = {
+  id: PropTypes.number.isRequired,
+  name: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  basePrice: PropTypes.number.isRequired,
+  colors: PropTypes.arrayOf(PropTypes.string).isRequired,
+  sizes: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string,
+      additionalPrice: PropTypes.number,
+    })
+  ).isRequired,
 };
 
 export default Product;
